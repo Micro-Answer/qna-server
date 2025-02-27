c63d8c7799dc677bf244f8e81775faae231eee01
glass <silver20190204@gmail.com>
2025-02-27 13:54:58 +0900

    feat: 컨트롤러에서 질문 등록 코드 추가

--- src/main/kotlin/com/example/rag/web/RagController.kt
+++ src/main/kotlin/com/example/rag/web/RagController.kt
@@ -1,6 +1,7 @@
 package com.example.rag.web
 
 import com.example.rag.qna.event.QnaSystem
+import com.example.rag.qna.event.QuestionEvent
 import com.example.rag.web.request.OpinionRequest
 import com.example.rag.web.request.QuestionRequest
 import org.springframework.http.HttpStatus
@@ -11,10 +12,11 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/v1/api/rag")
 class RagController(private val qnaSystem: QnaSystem) {
     @PostMapping("/questions")
-    fun enrollQuestion(@RequestBody request: QuestionRequest): ResponseEntity<String> {
-        TODO()
-        return ResponseEntity.status(HttpStatus.CREATED).body("${request.title} 등록 성공!")
-    }
+    fun enrollQuestion(@RequestBody request: QuestionRequest): ResponseEntity<String> =
+        request.run {
+            qnaSystem.recordEvent(QuestionEvent.create(title, category, content, userId))
+            ResponseEntity.status(HttpStatus.CREATED).body("${request.title} 등록 성공!")
+        }
 
     @PostMapping("/opinions")
     fun enrollOpinion(@RequestBody request: OpinionRequest): ResponseEntity<String> {
