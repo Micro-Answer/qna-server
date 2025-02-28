c6a037aeaf80bca0b0653a3d5813f33f4ff65b9b
glass <silver20190204@gmail.com>
2025-02-28 23:25:29 +0900

    feat: 의견 등록, 수정, 삭제 추가

--- src/main/kotlin/com/example/rag/web/RagController.kt
+++ src/main/kotlin/com/example/rag/web/RagController.kt
@@ -1,6 +1,7 @@
 package com.example.rag.web
 
 import com.example.rag.qna.event.QnaSystem
+import com.example.rag.qna.event.opinion.OpinionEvent
 import com.example.rag.qna.event.question.QuestionEvent
 import com.example.rag.web.request.OpinionRequest
 import com.example.rag.web.request.QuestionRequest
@@ -42,7 +43,30 @@ class RagController(private val qnaSystem: QnaSystem) {
     fun enrollOpinion(
         @RequestParam questionId: UUID,
         @RequestBody request: OpinionRequest
-    ): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.CREATED).body("${request.title} 등록 성공!")
+    ): ResponseEntity<String> =
+        request.run {
+            qnaSystem.recordEvent(OpinionEvent.create(title, content, userId, questionId))
+            ResponseEntity.status(HttpStatus.CREATED).body("${request.title} 등록 성공!")
+        }
+
+    @PutMapping("/opinions/{opinionId}")
+    fun updateOpinion(
+        @PathVariable opinionId: UUID,
+        @RequestParam questionId: UUID,
+        @RequestBody request: OpinionRequest
+    ): ResponseEntity<String> =
+        request.run {
+            qnaSystem.recordEvent(OpinionEvent.update(title, content, userId, questionId, opinionId))
+            ResponseEntity.ok("${request.title} 수정 성공!")
+        }
+
+    @DeleteMapping("/opinions/{opinionId}")
+    fun deleteOpinion(
+        @PathVariable opinionId: UUID,
+        @RequestParam questionId: UUID,
+        @RequestParam userId: String
+    ): ResponseEntity<Void> {
+        qnaSystem.recordEvent(OpinionEvent.delete(opinionId, userId, questionId))
+        return ResponseEntity.noContent().build()
     }
 }
